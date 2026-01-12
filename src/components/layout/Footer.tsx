96e87b49bdf070173b5c5113eeaf130be8b6f16f
donovanchen <donovanachen@gmail.com>
2026-01-12 12:04:44 -0800

    footer icons

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,20 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 function Footer() {
   return (
-    <div>
-      <button className="button">
-        <a href="https://github.com/DonChenn" target="_blank">
-          {" "}
-          GitHub
-        </a>
-      </button>
-
-      <button className="button">
-        <a href="https://www.linkedin.com/in/donovan-chen" target="_blank">
-          {" "}
-          LinkedIn
-        </a>
-      </button>
-    </div>
+    <footer>
+      <div className="footer-container">
+        <p className="footer-text">Stay in touch</p>
+        <div className="icon-container">
+          <a className="icon-link" href="https://www.linkedin.com/in/donovan-chen/" target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+          </a>
+          <a className="icon-link" href="https://github.com/DonChenn" target="_blank">
+            <FontAwesomeIcon icon={faGithub} size="2xl" />
+          </a>
+          <a className="icon-link" href="mailto:DonovanAChen@email.com">
+            <FontAwesomeIcon icon={faEnvelope} size="2xl" />
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 }
 
