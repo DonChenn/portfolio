a0db3757cf4e6520a2276c6d60a329e5b02592c6
donovanchen <donovanachen@gmail.com>
2026-01-17 19:40:38 -0800

    about me update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,7 +7,7 @@ function About() {
         student pursuing a B.S in Computer Science with a specialization in
         Intelligent Systems! I have a strong interest in full-stack development.
         I am driven by a desire for continuous learning, and I am always seeking
-        new opportunities to refine my skills. Outside of my professional
+        new growth opportunities. Outside of my professional
         interests, you can find me getting boba, playing basketball, or even
         with my film camera taking pictures!
       </p>
