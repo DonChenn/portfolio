479e0739767b55893a338e02fbca1244aebbd336
donovanchen <donovanachen@gmail.com>
2026-02-21 17:47:36 -0800

    footer styling

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,26 +5,34 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 function Footer() {
   return (
     <footer>
+      <hr className="footer-line" />
       <div className="footer-container">
-        <p className="footer-text">Stay in touch</p>
-        <div className="icon-container">
-          <a
-            className="icon-link"
-            href="https://www.linkedin.com/in/donovan-chen/"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faLinkedin} size="2xl" />
-          </a>
-          <a
-            className="icon-link"
-            href="https://github.com/DonChenn"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faGithub} size="2xl" />
-          </a>
-          <a className="icon-link" href="mailto:DonovanAChen@email.com">
-            <FontAwesomeIcon icon={faEnvelope} size="2xl" />
-          </a>
+        
+        <div className="footer-left">
+          <p className="made-by">Made by Donovan Chen</p>
+        </div>
+
+        <div className="footer-right">
+          <p className="footer-text">Stay in touch</p>
+          <div className="icon-container">
+            <a
+              className="icon-link"
+              href="https://www.linkedin.com/in/donovan-chen/"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+            </a>
+            <a
+              className="icon-link"
+              href="https://github.com/DonChenn"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faGithub} size="2xl" />
+            </a>
+            <a className="icon-link" href="mailto:DonovanAChen@email.com">
+              <FontAwesomeIcon icon={faEnvelope} size="2xl" />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
