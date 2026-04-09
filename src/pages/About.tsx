00fc486edaeba3e195b66e3a5edf0675f963b825
donovanchen <donovanachen@gmail.com>
2026-04-09 16:01:22 -0700

    added tama image

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,18 +1,19 @@
 function About() {
-  return (
-    <div className="About">
-      <h1>About Me</h1>
-      <p>
-        Hello! My name is Donovan. I am a fourth-year UC Irvine student pursuing
-        a B.S in Computer Science with a specialization in Intelligent Systems!
-        I have a strong interest in full-stack development. I am driven by a
-        desire for continuous learning, and I am always seeking new growth
-        opportunities. Outside of my professional interests, you can find me
-        getting boba, playing basketball, or even with my film camera taking
-        pictures!
-      </p>
-    </div>
-  );
+    return (
+        <div className="About">
+            <h1>About Me</h1>
+            <p>
+                Hello! My name is Donovan. I am a fourth-year UC Irvine student
+                pursuing a B.S in Computer Science with a specialization in
+                Intelligent Systems! I have a strong interest in full-stack
+                development. I am driven by a desire for continuous learning,
+                and I am always seeking new growth opportunities. Outside of my
+                professional interests, you can find me getting boba, playing
+                basketball, or even with my film camera taking pictures!
+            </p>
+            <img src="/portfolio/tama.png" alt="tama" width="500" />
+        </div>
+    );
 }
 
 export default About;
